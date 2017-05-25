50acf6cfe4b505cc8b4e5de6f200ddef83fa68ef
whung1 <wilson.Kainami@gmail.com>
2017-05-25 13:42:23 -0700

    initial commit intersection_of_two_arrays

---
+++ Leetcode/Algorithms/349_intersection_of_two_arrays.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/intersection-of-two-arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
+"""
+
+
+def intersection(nums1, nums2):
+    return list(set(nums1) & set(nums2))
+
+
+if __name__ == '__main__':
+    print(intersection([1,2,2,1],[2,2]))
