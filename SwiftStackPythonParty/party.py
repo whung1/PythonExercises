e7c8711ec785f346b55bc5084bee3fbe6ae59fae
whung1 <wilson.Kainami@gmail.com>
2017-07-15 13:18:53 -0700

    global variable refactoring into class EmployeeTree

--- SwiftStackPythonParty/party.py
+++ SwiftStackPythonParty/party.py
@@ -1,18 +1,27 @@
 import json
 
-party_list = []
 
-
-class Node(object):
+class EmployeeNode:
     def __init__(self, p):
         self.val = p['party-animal-score']
         self.name = p['name']
         self.children = []
         self.sum = None
-        self.inclusive = None
+        self.inclusive = None  # Boolean flag on whether this node is better with inclusive sum
 
     def add_child(self, child):
         self.children.append(child)
+
+    def get_grandchildren(self):
+        """
+        Returns list of grandchildren to this node
+        :rtype: List
+        """
+        grandchildren = []
+        if self.children is not None:
+            for child in self.children:
+                grandchildren.extend(child.children)
+        return grandchildren
 
     def is_inclusive(self):
         """
@@ -32,21 +41,10 @@ class Node(object):
             self.__init_memoization()
         return self.sum
 
-    def get_grandchildren(self):
-        """
-        Returns list of grandchildren to this node
-        :rtype: List
-        """
-        grandchildren = []
-        if self.children is not None:
-            for child in self.children:
-                grandchildren.extend(child.children)
-        return grandchildren
-
     def __init_memoization(self):
         """
         Private helper function to calculate the best way to sum party_animal_score
-        for the first time
+        for the first time and whether this node is better off with inclusive sum or not
         :rtype: List
         """
         exclusive = self.__get_exclusive_sum()
@@ -67,7 +65,7 @@ class Node(object):
 
     def __get_exclusive_sum(self):
         """
-        Party_animal_score summation method without including this node's val
+        Party_animal_score summation method excluding this node's val
         :return:
         """
         exclusive_sum = 0
@@ -76,38 +74,59 @@ class Node(object):
         return exclusive_sum
 
 
-def populate_party_list(n):
-    if n is None:
-        return
-    if n.val > 0 and n.is_inclusive():
-        party_list.append(n.name)
-        for grandchild in n.get_grandchildren():
-            populate_party_list(grandchild)
-    else:
-        for child in n.children:
-            populate_party_list(child)
+class EmployeeTree:
+    """
+    Representation of the employees at a place where nodes' parents are bosses, and children
+    are subordinates.
+    """
+    def __init__(self, json_list):
+        """
+        Create tree of subordinates to bosses
+        :param json_list: list of dicts of keys 'name', 'boss', 'party-animal-score' (See sample.json)
+        """
+        self.root = None
+        self.party_list = []
+        # Populate Tree with Nodes
+        nodes = dict((p['name'], EmployeeNode(p)) for p in json_list)
+        for p in json_list:
+            if p['boss'] is not None:
+                nodes[p['boss']].add_child(nodes[p['name']])
+            else:  # has no boss = is root / CEO
+                self.root = nodes[p['name']]
+
+    def get_party_list(self, ceo_required=False):
+        """
+        :param ceo_required: True if CEO is required in invite party list, False otherwise
+        :return: List of invited members to party maximizing party-animal-score
+        """
+        self.party_list = []
+        if ceo_required:
+            self.root.inclusive = True  # To get required CEO, just ensure they are included by flag
+        else:
+            self.root.inclusive = None  # In case we need to reset flag on subsequent usage
+        # TODO(wilson): change return to something more functional paradigm-esque while ensuring tail recursion
+        self.__populate_party_list(self.root)
+        return self.party_list
+
+    def __populate_party_list(self, n):
+        if n is None:
+            return
+        if n.val > 0 and n.is_inclusive():
+            self.party_list.append(n.name)
+            for grandchild in n.get_grandchildren():
+                self.__populate_party_list(grandchild)
+        else:
+            for child in n.children:
+                self.__populate_party_list(child)
 
 
 if __name__ == '__main__':
     print('Input JSON Filepath:')
     with open(input(), 'r') as f:
-        company_list = json.load(f)
-
-        # Create tree of subordinate children to bosses
-        root = None
-        nodes = dict((p['name'], Node(p)) for p in company_list)
-        for p in company_list:
-            if p['boss'] is not None:
-                nodes[p['boss']].add_child(nodes[p['name']])
-            else:  # has no boss = is CEO
-                root = nodes[p['name']]
+        employee_tree = EmployeeTree(json.load(f))
 
         # Max party-animal-score
-        populate_party_list(root)
-        print('Party List: {0}'.format(party_list))
+        print('Party Invite List: {0}'.format(employee_tree.get_party_list()))
 
         # CEO Required
-        party_list = []
-        root.inclusive = True  # To get required CEO, just ensure they are included by setting flag
-        populate_party_list(root)
-        print('Party List (CEO Required): {0}'.format(party_list))
+        print('Party Invite List (CEO Required): {0}'.format(employee_tree.get_party_list(ceo_required=True)))
